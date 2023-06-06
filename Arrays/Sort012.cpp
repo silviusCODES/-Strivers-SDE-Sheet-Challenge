112deb59e044ad7dc55a24ee76826db7e6881378
soumoodeep maity <94157324+silviusCODES@users.noreply.github.com>
2023-06-06 10:27:23 +0530

    Add files via upload

---
+++ Arrays/Sort012.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+
+   int low =0, mid =0 , high = n - 1;
+
+   while(mid <= high){
+      if(arr[mid] == 0)
+         swap(arr[mid++], arr[low++]);
+
+      else if(arr[mid] == 1)
+         mid++;
+
+      else
+         swap(arr[mid], arr[high--]);
+   }
+
+
+
+}
